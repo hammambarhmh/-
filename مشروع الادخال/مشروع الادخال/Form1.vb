439b2974d932f54d2b387ab2e1d49fdd6e7417f7
همام صبري بارحمة <hammambarhmh10@gmail.com>
2025-09-25 12:47:18 +0300

    Add files via upload

---
+++ مشروع الادخال/مشروع الادخال/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+
+    Dim x, y, w, k As String
+
+    Dim i As Integer
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        x = "هل تريد الخروج من الخروج؟ " + w
+        y = "تحذير "
+
+        MsgBox(x, MsgBoxStyle.YesNo + MsgBoxStyle.Critical + MsgBoxStyle.MsgBoxRight, y)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        i = InputBox("الرجاء أدخال العدد بين ال15 و الصفر", "صندوق الادخال")
+
+        If i >= 0 And i <= 15 Then
+            Me.BackColor = System.Drawing.ColorTranslator.FromOle(QBColor(Val(i)))
+
+        Else
+
+            MsgBox("ألادخال خطاء", "تنبيه")
+
+
+
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Randomize()
+
+        Me.BackColor = System.Drawing.ColorTranslator.FromOle(RGB(Rnd() * 255, Rnd() * 255, Rnd() * 255))
+
+
+
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        w = Chr(13) + Chr(10)
+
+        TextBox1.Text = "الحمدلله" + w + "رب العالمين"
+        Label1.Text = "الحمدلله" + w + "رب العالمين"
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        w = Chr(13) + Chr(10)
+
+        x = "كلية الحاسوب" + w
+        x = "ايتي" + w
+        x = "2025.2026 العام الدراسي " + w
+        y = "المشروع"
+
+        MsgBox(x, MsgBoxStyle.Information + MsgBoxStyle.MsgBoxRight, y)
+
+    End Sub
+End Class
---
+++ مشروع الادخال/مشروع الادخال/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("مشروع الادخال")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("مشروع الادخال")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5b44eaea-36a2-4029-8497-c9bede48d593")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
